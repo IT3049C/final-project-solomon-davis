fb6ea91238a5dbbd4e6147898946a7d192833e2a
Solo <233157871+daviswk90@users.noreply.github.com>
2025-11-29 16:40:00 -0500

    git add .&& git commit -m "Added landing page to display and filter games (WIP on developers)"

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,90 @@
+import { useSearchParams } from "react-router-dom";
+
+
+export function LandingPage() {
+    const [parms, setParms] = useSearchParams();
+    const searchGames = (parms.get("search") || "").toLowerCase();
+
+    const searchDevs = (parms.get("search") || "").toLowerCase();
+    const developers = [
+        {
+            key: "daviswk90",
+            name: "daviswk90",
+            games: ["rps, tic-tac-toe"]
+        },
+
+        {
+            key: "solomon davis",
+            name: "Solomon Davis",
+            games: ["wordle"]
+        }
+    ];
+
+    const games = [
+        {
+            key: "rps",
+            name: "Rock Paper Scissors",
+            description: "Play a simple game of Rock Paper Scissors!",
+            developer: `${developers.name}`
+
+        },
+
+        {
+            key: "tic-tac-toe",
+            name: "Tic-Tac-Toe",
+            description: "Play a simple game of Tic-Tac-Toe!",
+            developer: `${developers.name}`
+        },
+
+        {
+            key: "wordle",
+            name: "Wordle",
+            description: "Play an exciting word matching game to test your vocabulary and get the highest score!",
+            developer: `${developers.name}`
+        },
+
+        {
+            //One additional game of my choice
+            key: "",
+            name: "",
+            description: "",
+            developer: "",
+        }
+    ];
+
+    {/**filter the listed games based off if the game does equal the search, or change the game name to lowercase and see if it includes the search */}
+    const filteredGames = games.filter((game) => (!searchGames) || game.name.toLowerCase().includes(searchGames));
+
+    const filteredDevs = developers.filter((dev) => (!searchDevs) || dev.name.toLowerCase().includes(searchDevs));
+    
+    return (
+        <section>
+            <h2>Available Games</h2>
+            <p>Choose a game:</p>
+
+            <input 
+                id="game-search"
+                type="text"
+                placeholder="Search games..."
+                value={searchGames || searchDevs}
+                onChange={(e) => {
+                    const value = e.target.value;
+                    if (value.trim() === "") {
+                        setParms({});
+                    } else {
+                        setParms({search: value});
+                    }
+                }} />
+
+            <ul style={{textAlign: "left"}}>
+                {filteredGames.map((game) => (
+                    <li key={game.key}>
+                        <Link to={`/game/${game.key}`}>{game.name}</Link>
+                        <p>Description: {game.description}</p>
+                        <p>Developed By: {game.developer}</p>
+                    </li>
+                ))}
+            </ul>
+        </section>
+        )
+}
