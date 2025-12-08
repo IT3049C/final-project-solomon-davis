c884b6c2dd03a87d14f6100c2adc4c4bcb5eda98
Solo <233157871+daviswk90@users.noreply.github.com>
2025-12-07 23:32:54 -0500

    git commit -m "Trying to fix 404 Error"

--- src/components/navigation/ProtectedRoute.jsx
+++ src/components/navigation/ProtectedRoute.jsx
@@ -1,30 +1,14 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { loadSettings } from "../../logic/settings";
-import { useState, useEffect } from "react";
 
 export function ProtectedRoute({children}) {
 
-    const [hasSettings, setSettings] = useState(() =>loadSettings());
+    const hasSettings = loadSettings();
     const location = useLocation();
 
-    useEffect(() => {
-        const settingsSync = () => {
-            setSettings(loadSettings());
-        };
-
-        window.addEventListener("storage", settingsSync);
-            return () => window.removeEventListener("storage", settingsSync);
-        }, []
-    );
-
-    if (children === !hasSettings) {{/**If settings are not loaded */}
+    if (!hasSettings) {{/**If settings are not loaded */}
         {/**Navigate back to the lobby and change the state from the current location pathname to a different one (/lobby)*/}
-        return (
-            <>
-        <h3>Settings Not Saved</h3>
-        <Navigate to="/settings" state={{from: location.pathname}} replace /> 
-            </>
-    );
+        return <Navigate to="/lobby" state={{from: location.pathname}} replace />;
     }
     {/**Otherwise, go to the directed pathway (/game/rps or /game/tic-tac-toe) and load their elements*/}
     return children;
