0aaea4afd3dce148c6d4b46bb22a8ff0ce2e5c5b
Solo <233157871+daviswk90@users.noreply.github.com>
2025-12-07 19:16:49 -0500

    git commit -m "Attempting to fix protected route"

--- src/components/navigation/ProtectedRoute.jsx
+++ src/components/navigation/ProtectedRoute.jsx
@@ -1,14 +1,30 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { loadSettings } from "../../logic/settings";
+import { useState, useEffect } from "react";
 
 export function ProtectedRoute({children}) {
 
-    const hasSettings = loadSettings();
+    const [hasSettings, setSettings] = useState(() =>loadSettings());
     const location = useLocation();
 
-    if (!hasSettings) {{/**If settings are not loaded */}
+    useEffect(() => {
+        const settingsSync = () => {
+            setSettings(loadSettings());
+        };
+
+        window.addEventListener("storage", settingsSync);
+            return () => window.removeEventListener("storage", settingsSync);
+        }, []
+    );
+
+    if (children === !hasSettings) {{/**If settings are not loaded */}
         {/**Navigate back to the lobby and change the state from the current location pathname to a different one (/lobby)*/}
-        return <Navigate to="/settings" state={{from: location.pathname}} replace />;
+        return (
+            <>
+        <h3>Settings Not Saved</h3>
+        <Navigate to="/settings" state={{from: location.pathname}} replace /> 
+            </>
+    );
     }
     {/**Otherwise, go to the directed pathway (/game/rps or /game/tic-tac-toe) and load their elements*/}
     return children;
